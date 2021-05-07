67518c25c2850cf72f98cab89012827724076cc1
VedaantKuchhal <vedaantk22@gmail.com>
2021-05-07 06:54:11 -0400

    Add method docsrings and comment code.

--- player_model.py
+++ player_model.py
@@ -1,5 +1,15 @@
-from settings import *
+"""
+Create a player for the PlatFroger game.
+"""
 import pygame as pg
+from settings import (BLACK,
+                      HEIGHT,
+                      WIDTH,
+                      PLAYER_JUMP,
+                      PLAYER_ACC,
+                      PLAYER_GRAVITY,
+                      PLAYER_FRICTION,)
+
 from game_view import GameView
 # Import vectors from pygame math module.
 vec = pg.math.Vector2
@@ -25,7 +35,10 @@ class Player(pg.sprite.Sprite):
 
     def __init__(self, game_model):
         """
-        Initializes the Player class
+        Initialize the Player class.
+
+        Args:
+            game_model: An instance of a game model.
         """
 
         # Inherit all attributes of pygame Sprite class.
@@ -33,8 +46,11 @@ class Player(pg.sprite.Sprite):
 
         # Pass an instance of the game to the player sprite.
         self.game_model = game_model
+
+        # Initialize an instance of the game view.
         self.game_view =  GameView(game_model)
-        #animation attributes
+
+        # Animation attributes.
         self.walking = False
         self.jumping = False
         self.current_frame = 0
@@ -44,8 +60,6 @@ class Player(pg.sprite.Sprite):
         self.image = self.game_view.spritesheet.get_image(83, 346, 64, 50)
         self.image.set_colorkey(BLACK)
         self.game_view.load_images()
-
-        #self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
 
         # Initialize sprite's position, velocity, and acceleration vectors.
@@ -55,11 +69,9 @@ class Player(pg.sprite.Sprite):
         self.acc = vec(0, 0)
 
 
-
-
     def jump(self):
         """
-        Executes necessary actions for jumping movement of player (jump sound,
+        Execute necessary actions for jumping movement of player (jump sound,
         moving, etc.)
         """
         # Check if player sprite is on a platform.
@@ -73,13 +85,27 @@ class Player(pg.sprite.Sprite):
 
     def jump_cut(self):
         """
-        Makes the minimum jump velocity -3 for a more natural movement
+        Allow the player to vary jump height by doing shorter hops if the jump
+        key is pressed and released quickly.
         """
         if self.jumping:
             if self.vel.y < -3:
                 self.vel.y = -3
 
     def move(self, direction):
+        """
+        Inform left and right movement of the player sprite based on the input
+        message.
+
+        The player's motion is controlled by changing the player's acceleration
+        in order to give the player a more realistic motion when moving left
+        and right.
+
+        Args:
+            direction: A string that is either "Stop", "Left", or "Right" that
+            informs the player sprite of the direction it is supposed to
+            accelerate.
+        """
         if direction == "Stop":
             self.acc.x = 0
         if direction == "Left":
@@ -88,9 +114,17 @@ class Player(pg.sprite.Sprite):
             self.acc.x = PLAYER_ACC
 
     def move_test(self):
+        """
+        Control movement of player using arrow keys.
+
+        Pressing the left arrow key results in a message to go left, while
+        pressing the right arrow key results in a message to go right. Pressing
+        both results in the player coming to a halt.
+        """
+        # Obtain all the current key presses.
         keys = pg.key.get_pressed()
 
-
+        # Match key presses to corresponding direction messages.
         if keys[pg.K_LEFT]:
             self.move("Left")
 
@@ -102,19 +136,19 @@ class Player(pg.sprite.Sprite):
 
     def update(self):
         """
-        Updates the player in the game loop
+        Update the player in the game loop.
+
+        Define the 'laws of physics' that the player sprite must follow, make
+        the sprite wrap around the screen when necessary, and update its
+        position accordingly.
         """
+        # Run the animation for the player sprite.
         self.animate()
 
         # Include gravity in the game.
         self.acc = vec(0, PLAYER_GRAVITY)
 
-        # keys = pg.key.get_pressed()
-        # if keys[pg.K_LEFT]:
-        #     self.acc.x = -PLAYER_ACC
-        #
-        # if keys[pg.K_RIGHT]:
-        #     self.acc.x = PLAYER_ACC
+        # Move the player left and right.
         self.move_test()
 
         # Instill friction into movement.
@@ -140,34 +174,50 @@ class Player(pg.sprite.Sprite):
 
     def animate(self):
         """
-        Animates characters
+        Animate player sprite.
+
+        Define what the sprite will look like in different states, i.e. if it
+        is jumping or walking, and which direction it is pointing in.
         """
-        #walking conditions
+        # Initialize timer (to be used later).
         now = pg.time.get_ticks()
+
+        # Decide if player is walking.
         if abs(self.vel.x) >= 0.5:
             self.walking = True
         else:
             self.walking = False
 
-        # Jumping animation
+        # Jumping animation.
         if self.jumping:
-            bottom = self.rect.bottom
-            if self.vel.x<0:
+            # bottom = self.rect.bottom
+
+            # Choose left or right jumping frame depending on the player's
+            # horizontal velocity.
+            if self.vel.x < 0:
                 self.image = self.game_view.jump_l
             else:
                 self.image = self.game_view.jump_r
-        #Walking animation
+
+        # Walking animation.
         elif self.walking:
+            # Alternate between two walking animation frames every 100
+            # milliseconds to produce a realistic walking motion.
             if now - self.last_update > 100:
                 self.last_update = now
-                self.current_frame = (self.current_frame + 1) % len(self.game_view.walk_frames_l)
-                bottom = self.rect.bottom
-                if self.vel.x<0:
-                    self.image = self.game_view.walk_frames_l[self.current_frame]
+                self.current_frame = (self.current_frame + 1) \
+                % len(self.game_view.walk_frames_l)
+                # bottom = self.rect.bottom
+                # Choose left or right walking frame based on the player's
+                # horizontal velocity.
+                if self.vel.x < 0:
+                    self.image = \
+                    self.game_view.walk_frames_l[self.current_frame]
                 else:
-                    self.image = self.game_view.walk_frames_r[self.current_frame]
+                    self.image = \
+                    self.game_view.walk_frames_r[self.current_frame]
 
-        #Make sure jumping and walking sprites are in the right direction
+        # Ensure that jumping and walking sprites are in the same direction.
         else:
             if self.image == self.game_view.jump_l:
                 self.image = self.game_view.walk_frames_l[self.current_frame]
